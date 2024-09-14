57c598c9df2c128c2f6e2df9a897961b721cdc7d
lntvow <lntvow@qq.com>
2024-09-14 09:40:54 +0800

    1111

--- src/libs/amap-loader/index.js
+++ src/libs/amap-loader/index.js
@@ -5,55 +5,73 @@ window._AMapSecurityConfig = {
   securityJsCode: 'e5a1fc65e4e87debf400098d524e12d6',
 }
 
-// const getLocation = () => {
-//   return new Promise((resolve, reject) => {
-//     // 获取AMap实例
-//     AMapLoader.load({
-//       key: '39371e6e81984c16294bb4aea1351304',
-//       version: '2.0',
-//       plugins: ['AMap.Geolocation'],
-//     })
-//       .then(AMap => {
-//         const geolocation = new AMap.Geolocation({
-//           // 逆地理编码配置
-//           needAddress: true
-//         })
-//         // 获取逆编码后地址信息
-//         geolocation.getCurrentPosition((status, result) => {
-//           let location = {
-//             longitude: '',
-//             latitude: '',
-//             provinceCode: '',
-//             province: '',
-//             cityCode: '',
-//             city: '',
-//             adCode: ''
-//           }
-//           if (status === 'complete') {
-//             const { province, citycode, city, adcode } = result.addressComponent
-//             const { lng, lat } = result.position
-//             location = {
-//               longitude: lng,
-//               latitude: lat,
-//               provinceCode: adcode.slice(0, 2) + '0000',
-//               province: province,
-//               cityCode: citycode,
-//               city: city,
-//               adCode: adcode
-//             }
-//             console.log('111', location)
-//             resolve(location)
-//           } else {
-//             console.log('解析定位失败', result.message)
-//             reject(result.message)
-//           }
-//         })
-//       })
-//       .catch(err => {
-//         console.log('AMap对象获取失败:', err)
-//       })
-//   })
-// }
+let AMap
+
+async function init() {
+  AMap = await AMapLoader.load({
+    key: '39371e6e81984c16294bb4aea1351304',
+    version: '2.0',
+    plugins: ['AMap.Geolocation', 'AMap.Weather'],
+  })
+}
+
+//
+
+async function wrapper(fun) {
+  if (AMap) {
+    return fun()
+  } else {
+    await init()
+    return fun()
+  }
+}
+
+const getLocation2 = () => {
+  return wrapper(() => {
+    11111111
+  })
+}
+const getLocation = () => {
+  return wrapper(async () => {
+    let { promise, resolve, reject } = Promise.withResolvers()
+
+    const geolocation = new AMap.Geolocation({
+      // 逆地理编码配置
+      needAddress: true,
+    })
+    // 获取逆编码后地址信息
+    geolocation.getCurrentPosition((status, result) => {
+      let location = {
+        longitude: '',
+        latitude: '',
+        provinceCode: '',
+        province: '',
+        cityCode: '',
+        city: '',
+        adCode: '',
+      }
+      if (status === 'complete') {
+        const { province, citycode, city, adcode } = result.addressComponent
+        const { lng, lat } = result.position
+        location = {
+          longitude: lng,
+          latitude: lat,
+          provinceCode: adcode.slice(0, 2) + '0000',
+          province: province,
+          cityCode: citycode,
+          city: city,
+          adCode: adcode,
+        }
+
+        resolve(location)
+      } else {
+        reject(result.message)
+      }
+    })
+
+    return promise
+  })
+}
+//
 
 export { getLocation }
-
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
-  import CustomNav from '@/components/CustomNav/CustomNav.vue'
-  import CustomSwiper from '@/components/CustomSwiper/CustomSwiper.vue'
-  import UserInfo from './components/UserInfo.vue'
-  import QuickLink from './components/QuickLink.vue'
-  import NearbyService from './components/NearbyService.vue'
-  import CustomTab from '@/components/CustomTab/CustomTab.vue'
-  import { onLoad } from '@dcloudio/uni-app'
-  import { getbannerList, getCityList } from '@/api/common'
-  import { storeToRefs } from 'pinia'
-  import { useUserStore } from '@/store/user'
-  import { getUserInfo } from '@/api/user'
-  import { useCacheStore } from '@/store/cache'
-  import { getLocation } from '@/libs'
+import CustomNav from '@/components/CustomNav/CustomNav.vue'
+import CustomSwiper from '@/components/CustomSwiper/CustomSwiper.vue'
+import UserInfo from './components/UserInfo.vue'
+import QuickLink from './components/QuickLink.vue'
+import NearbyService from './components/NearbyService.vue'
+import CustomTab from '@/components/CustomTab/CustomTab.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { getbannerList, getCityList } from '@/api/common'
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '@/store/user'
+import { getUserInfo } from '@/api/user'
+import { useCacheStore } from '@/store/cache'
+import { getLocation } from '@/libs'
 
-  const { storeUserInfo, storeIsLogin } = storeToRefs(useUserStore())
-  // const { storeBannerList } = storeToRefs(useCacheStore())
+const { storeUserInfo, storeIsLogin } = storeToRefs(useUserStore())
+// const { storeBannerList } = storeToRefs(useCacheStore())
 
-  onLoad(async () => {
-    if (storeIsLogin.value) {
-      const data = await getUserInfo()
+onLoad(async () => {
+  if (storeIsLogin.value) {
+    const data = await getUserInfo()
 
-      storeUserInfo.value = data
-    }
-    // getbannerList().then(res => {
-    //   storeBannerList.value = res
-    // })
+    storeUserInfo.value = data
+  }
+  // getbannerList().then(res => {
+  //   storeBannerList.value = res
+  // })
 
-    // getLocation().then((data: any) => {
-    //   console.log('000', data)
-    // })
+  // getLocation().then((data: any) => {
+  //   console.log('000', data)
+  // })
 
-    getLocation()
+  getLocation().then(a => {
+    console.log('a: ', a)
   })
+})
 </script>
 
 <template>
@@ -46,9 +48,9 @@
 </template>
 
 <style scoped lang="scss">
-  .page {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
